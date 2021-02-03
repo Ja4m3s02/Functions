d3a3901cecc6912a1889807be451ffbb26510ca0
James Eland <s210209@students.aie.edu.au>
2021-02-03 14:55:58 +1100

    Hey

--- AIE_01_SyntaxFunction/AIE_04_Calculator/Program.cs
+++ AIE_01_SyntaxFunction/AIE_04_Calculator/Program.cs
@@ -4,9 +4,45 @@ namespace AIE_04_Calculator
 {
     class Program
     {
+        static int Calculate(int num1, int num2, string operation)
+        {
+            if (operation == "add")
+            {
+                return num1 + num2;
+            }
+            else if (operation == "sub")
+            {
+                return num1 - num2;
+            }
+            else if (operation == "mul")
+            {
+                return num1 * num2;
+            }
+            else if (operation == "div")
+            {
+                return num1 / num2;
+            }
+            else if (operation == "pow")
+            {
+                return (int)Math.Pow(num1, num2);
+            }
+
+            throw new Exception($"The operation {operation} is not supported");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an operation");
+            string op = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(Calculate(4, 3, op));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
