332d86bb1b3e73ced241cc9eda6fcf7ceaade78e
James Eland <s210209@students.aie.edu.au>
2021-02-08 08:53:19 +1100

    Hi

---
+++ AIE_02_IsAVowel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AIE_02_IsAVowel
+{
+    class Program
+    {
+        static bool IsAVowel(char c)
+        {
+            c = char.ToLower(c);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAVowel('A')); // should output true 
+            Console.WriteLine(IsAVowel('a')); // should output true 
+            Console.WriteLine(IsAVowel('b')); // should output false 
+            Console.WriteLine(IsAVowel('E')); // should output true 
+            Console.WriteLine(IsAVowel('e')); // should output true 
+            Console.WriteLine(IsAVowel('I')); // should output true 
+            Console.WriteLine(IsAVowel('i')); // should output true 
+            Console.WriteLine(IsAVowel('P')); // should output false
+        }
+    }
+}
---
+++ AIE_03_MaxOfThree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AIE_03_MaxOfThree
+{
+    class Program
+    {
+        static bool MaxOfThree(int a, int b, int c);
+
+
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine(MaxOfThree(6, 9, 1)); // should output 9 
+            Console.WriteLine(MaxOfThree(20, 20, 20)); // should output 20
+            Console.WriteLine(MaxOfThree(6, 1, 6)); // should output 6
+        }
+    }
+}
